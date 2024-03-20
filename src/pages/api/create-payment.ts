0d52ccd6c8f6505dd7a91c4e6d754e687cc73d7a
João Pedro Batista Santos <joaopbatistastos@gmail.com>
2024-03-20 10:38:08 -0300

    feat: test payment

--- src/pages/api/create-payment.ts
+++ src/pages/api/create-payment.ts
@@ -1,43 +1,27 @@
 import axios from 'axios';
-import { collection, doc, getDocs, query, updateDoc, where } from "firebase/firestore";
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { db } from '../../../firebase';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-   console.log("Recebendo requisição para create-payment", req.body);
+   console.log("Recebendo requisição para create-payment", req.body); // Loga os dados recebidos
 
    if (req.method === 'POST') {
-      const { email, ...paymentData } = req.body;
-
       try {
-         console.log("Enviando dados para ASAAS", paymentData);
-         const response = await axios.post('https://sandbox.asaas.com/api/v3/subscriptions/', paymentData, {
+         console.log("Enviando dados para ASAAS", req.body);
+         const { data } = await axios.post('https://sandbox.asaas.com/api/v3/subscriptions/', req.body, {
             headers: {
                'accept': 'application/json',
                'content-type': 'application/json',
-               'access_token': '$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MzdjNWZmNDQ6OjAwMDAwMDAwMDAwMDAwNzY2MzE6OiRhYWNoX2M5MzZkNzU3LTNjM2MtNDg1MC05OTlhLTVjMmZkNjM1ODFjZg==', // Substitua pelo seu token de acesso
+               'access_token': '$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MzdjNWZmNDQ6OjAwMDAwMDAwMDAwMDAwNzY2MzE6OiRhYWNoX2M5MzZkNzU3LTNjM2MtNDg1MC05OTlhLTVjMmZkNjM1ODFjZg==',
             },
          });
-         const { data } = response;
          console.log("Resposta do ASAAS", data);
-
-         const usersRef = collection(db, 'Clients');
-         const q = query(usersRef, where('email', '==', email));
-         const querySnapshot = await getDocs(q);
-
-         if (!querySnapshot.empty) {
-            querySnapshot.forEach(async (document) => {
-               const userDocRef = doc(db, 'Clients', document.id);
-               await updateDoc(userDocRef, { paymentId: data.id });
-            });
-
-            res.status(200).json(data);
-         } else {
-            throw new Error('Usuário não encontrado.');
-         }
+         res.status(200).json(data);
       } catch (error: any) {
          console.error('Erro na API create-payment:', error);
-         res.status(500).json({ message: 'Erro interno no servidor', details: error.response?.data || error.message });
+         if (error.response) {
+            console.error("Detalhes do erro:", error.response.data);
+         }
+         res.status(500).json({ message: 'Erro interno no servidor' });
       }
    } else {
       console.warn(`Método não permitido: ${req.method}`);
